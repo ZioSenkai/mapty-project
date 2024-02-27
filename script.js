b26eb2a827299f0686f3b5d70c5121d2117d6f63
Zio <143772055+ZioSenkai@users.noreply.github.com>
2024-02-27 21:58:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -20,7 +22,7 @@ if (navigator.geolocation) {
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
       // using the 3rd party Leaflet to add a map into our web app
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution:
@@ -28,9 +30,52 @@ if (navigator.geolocation) {
       }).addTo(map);
       const marker = L.marker(coords).addTo(map);
       marker.bindPopup('<b>Your current location!</b>').openPopup();
+
+      // Add event listener onto the map of the leaflet library
+      // Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  const newCoords = [lat, lng];
+  L.marker(newCoords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('WORKOUT')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  // inputDuration.placeholder = 'hour'; // we could change it to hour if we want
+});
